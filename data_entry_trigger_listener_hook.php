7f956c023539114423b3153e3f392c7b39cea1ef
prasadlan <prasad2311@gmail.com>
2017-07-10 16:26:38 -0400

    remove cps related code to fetch config data

--- data_entry_trigger_listener_hook.php
+++ data_entry_trigger_listener_hook.php
@@ -1,44 +1,38 @@
 <?php
 	return function($project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id, $repeat_instance){
-		//require_once "RXSender.php";
-		//require_once "send_rx_functions.php";
+		require_once "send_rx_functions.php";
+
 		$username = USERID;
 		parse_str($_SERVER['QUERY_STRING'], $qs_params);
 		$instrument = $qs_params['page'];
 		$redcap_event_name = REDCap::getEventNames(true, false, $event_id);
+
 		$get_data = REDCap::getData('JSON');
 		$decoded_data = json_decode($get_data);
 		$instrument_complete = $instrument.'_complete';
+
 		$is_instrument_complete = false;
 		$is_instrument_last = false;
 
 		/*
-			Check if the current instrument is the last instrument of the event.
-			1. Get all the data using getData developer method.
-			2. Get data for the current event.
-			3. Get list of all instruments from the data and store them in an array.
-			4. The instrument at the last index of the array is the last instrument of the event.
-			
-			Check if the current instrument is complete.
+			Check if current instrument is the last instrument of the event.
 		*/
-		$instrument_array = array();
+		global $Proj;
+		$arr = $Proj->eventsForms;
+		$last_key_index = end(array_keys($arr[$event_id]));
+		if($arr[$event_id][$last_key_index] == $instrument){
+			$is_instrument_last = true;
+		}
+
+		/*
+			Check if current instrument is complete.
+		*/
 		foreach ($decoded_data as $item) {
 			$unique_event_name = $item->redcap_event_name;
 			if($unique_event_name == $redcap_event_name){
-				foreach ($item as $key => $value) {
-					if(stripos('_complete', $key) !== false){
-						$instrument_array[] = $key;
-					}
-				}
-				$array_keys = array_keys($instrument_array);
-				$last_key = end($array_keys);
-				if($last_key == $instrument_complete){
-					$is_instrument_last = true;
-				}
 				if($instrument_complete == '2'){
 					$is_instrument_complete = true;
 				}
-
 			}
 		}
 
@@ -50,7 +44,7 @@
 		}
 
 		/*
-			Do not proceed if current instrument (i.e., last instrument) is not complete.
+			Do not proceed if current instrument (i.e., last instrument) is incomplete.
 		*/
 		if(!$is_instrument_complete){
 			return;
@@ -62,14 +56,8 @@
 			return;
 		}
 
-		class Sender extends RXSender{
 
-			function buildPrescriptionTable(){
-
-			}
-		}
-
-		$sender = new Sender($project_id, $config, $redcap_event_name, $record, $username); 
+		$sender = send_rx_get_sender($project_id, $config, $redcap_event_name, $record, $username); 
 
 		$isSecure = $sender->checkSecurityToken();
 		if($isSecure == FALSE){
