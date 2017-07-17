90c2d4bc7d302f6037d68cb92b4faa183b624738
prasadlan <prasad2311@gmail.com>
2017-07-17 16:21:14 -0400

    added helptext below save buttons for last instrument

--- send_rx_data_entry_form_hook.php
+++ send_rx_data_entry_form_hook.php
@@ -17,6 +17,17 @@
 				$is_pdf_generated = true;
 				send_rx_save_record_field($project_id, $event_id, $record_id, $field_name, $is_pdf_generated, $repeat_instance);
 			}
+			?>
+			<script type="text/javascript">
+				/*
+					All DOM modifications for the final instrument.
+				*/
+				document.addEventListener('DOMContentLoaded', function(){
+					var txt = '<div style="color: #666;font-size: 11px;"><span>NOTE: </span>New PDF will be generated and sent once the form is saved.</div>';
+					$(txt).insertBefore($('button[name="submit-btn-cancel"]')[0]);
+				});
+			</script>
+			<?php
 		}
 	};
 ?>
