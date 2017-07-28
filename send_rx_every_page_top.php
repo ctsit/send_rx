54c27ba8fd58e92934a402f4ca760245186a85b5
prasadlan <prasad2311@gmail.com>
2017-07-28 10:41:15 -0400

    format text

--- send_rx_every_page_top.php
+++ send_rx_every_page_top.php
@@ -1,5 +1,5 @@
 <?php
-	return function ($project_id){
+    return function ($project_id){
         $url = $_SERVER['REQUEST_URI'];
         parse_str($_SERVER['QUERY_STRING'], $qs_params);
         $event_id = $qs_params['event_id'];
@@ -15,9 +15,9 @@
             return;
         }
 
-		global $Proj;
+        global $Proj;
 
-		if (!$config = send_rx_get_project_config($project_id, 'pharmacy')) {
+        if (!$config = send_rx_get_project_config($project_id, 'pharmacy')) {
             return;
         }
         
@@ -30,7 +30,7 @@
         $users_formname = $Proj->metadata['send_rx_pharmacy_name']['form_name'];
         $proj_users = $data[$record]['repeat_instances'][$event_id][$form_name];
         foreach ($proj_users as $key => $item) {
-        	$users_in_dag[] = $item['send_rx_prescriber_id'];
+            $users_in_dag[] = $item['send_rx_prescriber_id'];
         }
         $encoded_users_in_dag = json_encode($users_in_dag);
         ?>
@@ -41,20 +41,20 @@
                 var group_id = '<?php echo $dag_id; ?>';
                 var usersInDag = '<?php echo $encoded_users_in_dag; ?>';
 
-        		// Assign users to DAG
+                // Assign users to DAG
                 $('#assign_access_btn').on('click', function(){
                     // Add each user to DAG
                     for(var i=0;i<usersInDag.length;i++){
-	                    $.get(app_path_webroot+'DataAccessGroups/data_access_groups_ajax.php?pid='+pid+'&action=add_user&user='+usersInDag[i]['username']+'&group_id='+group_id,{ },function(data){
-	                        if(data){
-	                            return;
-	                        }
-	                    });
-	                }
+                        $.get(app_path_webroot+'DataAccessGroups/data_access_groups_ajax.php?pid='+pid+'&action=add_user&user='+usersInDag[i]['username']+'&group_id='+group_id,{ },function(data){
+                            if(data){
+                                return;
+                            }
+                        });
+                    }
                 });
 
-				// Revoke access to users from DAG
-				$('#revoke_access_btn').on('click', function(){
+                // Revoke access to users from DAG
+                $('#revoke_access_btn').on('click', function(){
                     // Remove each user to DAG
                     for(var i=0;i<usersInDag.length;i++){
                         $.get(app_path_webroot+'DataAccessGroups/data_access_groups_ajax.php?pid='+pid+'&action=add_user&user='+usersInDag[i]['username']+'&group_id=""',{ },function(data){
@@ -63,9 +63,9 @@
                             }
                         });
                     }
-				});
+                });
             });
         </script>
         <?php
-	}
+    }
 ?>
