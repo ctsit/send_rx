6bd198364f6f36419f105a7f46a4403a235ef454
tbembersimeao <tbembersimeao@ufl.edu>
2018-01-16 10:54:05 -0500

    Fixing regression

--- ExternalModule.php
+++ ExternalModule.php
@@ -448,26 +448,16 @@ class ExternalModule extends AbstractExternalModule {
 
         // Checking if we are at the prescriber field's step.
         $id_field_name = 'send_rx_prescriber_id';
-        if (isset($Proj->metadata[$id_field_name]) && $_GET['page'] == $Proj->metadata[$id_field_name]['form_name']) {
-            if (empty($_POST[$id_field_name])) {
-                return;
-            }
+        $target_field_name = 'send_rx_prescriber_email';
 
-            $target_field_name = 'send_rx_prescriber_email';
-            if (!isset($Proj->metadata[$target_field_name])) {
-                return;
-            }
-
+        if (isset($Proj->metadata[$id_field_name]) && $_GET['page'] == $Proj->metadata[$id_field_name]['form_name'] && isset($Proj->metadata[$target_field_name]) && !empty($_POST[$id_field_name])) {
             $user_profile = new UserProfile($_POST[$id_field_name]);
             $data = $user_profile->getProfileData();
 
             $source_field_name = 'send_rx_user_email';
-            if (empty($data[$source_field_name])) {
-                return;
+            if (!empty($data[$source_field_name])) {
+                send_rx_save_record_field($project_id, $event_id, $record, $target_field_name, $data[$source_field_name]);
             }
-
-            send_rx_save_record_field($project_id, $event_id, $record, $target_field_name, $data[$source_field_name]);
-            return;
         }
 
         // Checking if PDF file exists.
